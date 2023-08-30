445fbf791f9816c309239e835cd0d921f3d20dd1
SkyTryX <dorian.gil34@gmail.com>
2023-08-30 09:21:56 +0200

    - Very small change

--- src/main/java/fr/skytryx/lobbysystem/LobbySystem.java
+++ src/main/java/fr/skytryx/lobbysystem/LobbySystem.java
@@ -6,6 +6,7 @@ import fr.skytryx.lobbysystem.events.DoubleJump;
 import fr.skytryx.lobbysystem.events.LobbyProtection;
 import fr.skytryx.lobbysystem.events.ScoreboardM;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -20,6 +21,17 @@ public final class LobbySystem extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new DoubleJump(this), this);
         getServer().getPluginManager().registerEvents(new LobbyProtection(), this);
         getServer().getPluginManager().registerEvents(new ScoreboardM(), this);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.getOnlinePlayers().forEach(player ->{
+                    if(player.getLocation().getBlockY() <= 0){
+                        player.teleport(new Location(Bukkit.getWorld("world"), 0, 64, 0));
+                    }
+                });
+            }
+        }, 40L, 40L);
     }
 
     @Override
--- src/main/java/fr/skytryx/lobbysystem/events/LobbyProtection.java
+++ src/main/java/fr/skytryx/lobbysystem/events/LobbyProtection.java
@@ -1,6 +1,8 @@
 package fr.skytryx.lobbysystem.events;
 
 import fr.skytryx.lobbysystem.commands.CommandBuild;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -46,6 +48,7 @@ public class LobbyProtection implements Listener {
 
     @EventHandler
     public void onJoinBuild(PlayerJoinEvent event){
+        event.getPlayer().teleport(new Location(Bukkit.getWorld("world"), 0, 64, 0));
         if(!CommandBuild.BuildMap.containsKey(event.getPlayer())){
             CommandBuild.BuildMap.put(event.getPlayer(), false);
         }
